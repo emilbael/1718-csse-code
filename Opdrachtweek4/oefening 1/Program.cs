d1419aa5aa97d318e1a18989f8c3570308611db9
emilbael <emilbael@student.arteveldehs.be>
2017-10-22 21:37:05 +0200

    Add files via upload

---
+++ Opdrachtweek4/oefening 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Opdrachtweek4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Geef een getal in tot en met 100: ");
+           int input = Convert.ToInt32(Console.ReadLine());
+           for(var a = 0; a <= input ;a++)
+           {
+               int delers = 0;
+                for (int i = 2; i <= a; i++)
+                if (a % i == 0)                        
+                delers++;
+
+                if (a != 1 && delers == 1)                 
+                Console.WriteLine(a + " is een priemgetal.");
+                else
+                Console.WriteLine(a + " is geen priemgetal.");
+               
+           }
+
+            
+        }
+    }
+}
---
+++ Opdrachtweek4/oefening 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace oefening_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geef een getal in: ");
+            int aantal=Convert.ToInt32(Console.ReadLine());
+            int fibo1 = 0;
+            int fibo2 = 1;
+
+            
+            Console.WriteLine(fibo1);
+            Console.WriteLine(fibo2);
+
+            for (int i = 3; i <=aantal; i++)
+            {
+                int fibo3 = fibo1 + fibo2;
+                fibo1 = fibo2;
+                fibo2 = fibo3;
+                Console.WriteLine(fibo3);
+            }
+        }
+    }
+}
